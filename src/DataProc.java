08b5030ff87f22acc05a8240d6a538c858693310
airam <airamhr7@gmail.com>
2020-02-21 11:23:28 +0100

    hello

--- src/DataProc.java
+++ src/DataProc.java
@@ -2,9 +2,13 @@ import java.util.List;
 
 public class DataProc {
 
+    private List<Point> points;
+
     /**Returns the points list**/
-    public List getPoint(){
-        return null;
+    public List<Point> getPoints(){
+        return points;
     }
+
+
 
 }
--- src/Point.java
+++ src/Point.java
@@ -1,4 +1,26 @@
 public class Point {
 
+    private Coord coord;
+    private int data; //[-100, 100]
 
+    public Point(Coord coord, int data){
+        this.coord = coord;
+        this.data = data;
+    }
+
+    public Coord getCoord() {
+        return coord;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setCoord(Coord coord) {
+        this.coord = coord;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
 }
