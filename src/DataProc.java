f574f03f1e008f5956611b37a69e3c3a481b5da4
airam <airamhr7@gmail.com>
2020-02-21 10:55:50 +0100

    hello

---
+++ src/DataProc.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class DataProc {
+
+    /**Returns the points list**/
+    public List getPoint(){
+        return null;
+    }
+
+}
---
+++ src/MapMaker.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class MapMaker {
+    /**Create the map**/
+    public Image getMap(){
+        return null;
+    }
+
+    /**Private methods**/
+
+
+
+}
