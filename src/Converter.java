6a40aa78bb5a69f316f6acdc5f32767750313c14
RodrigoLlanes <romadritex@gmail.com>
2020-02-21 15:41:07 +0100

    :)

--- src/Converter.java
+++ src/Converter.java
@@ -1,6 +1,6 @@
 public class Converter {
-    private final static Coord upvCoord = new Coord(729010.386599999852479 ,4373390.0215);
-    private final static Coord aytCoord = new Coord(725685.211699999869, 4372281.7883);
+    private final static Coord upvCoord = new Coord(39.480101,-0.344469);
+    private final static Coord aytCoord = new Coord(39.470319,-0.377101);
 
     private double despX;
     private double despY;
@@ -23,12 +23,9 @@ public class Converter {
     }
 
     public static void main(String[] args){
-        Coord upv = new Coord(729010.386599999852479 ,4373390.0215);
-        Coord ayt = new Coord(725685.211699999869, 4372281.7883);
-        Coord viv = new Coord(726236.403599999845028,4373308.10099999979);
-        Coord vivN = new Coord(39.480783, -0.367737);
-        Coord upvN = new Coord(39.479650, -0.337397);
-        Coord aytN = new Coord(39.470069, -0.377746);
+        Coord viv = new Coord(39.480936,-0.367661);
+        Coord upvN = new Coord(2.5, 15);
+        Coord aytN = new Coord(5, 10);
 
         Converter c = new Converter(upvN, aytN);
         Coord n = c.convert(viv);
