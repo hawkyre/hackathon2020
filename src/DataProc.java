d77714e689e31977a5cc43afa540c42287782cc2
airam <airamhr7@gmail.com>
2020-02-21 16:38:18 +0100

    hello

--- src/DataProc.java
+++ src/DataProc.java
@@ -9,7 +9,7 @@ public class DataProc {
     private ArrayList<Point> points;
 
     public DataProc(){
-        points = readCsv();
+        points = readCSV();
     }
 
     /**Returns the points list**/
@@ -17,9 +17,8 @@ public class DataProc {
         return points;
     }
 
-    public ArrayList<Point> readCsv(){
+    public ArrayList<Point> readCSV(){
         ArrayList<Point> pointList = new ArrayList<>();
-
         String csvFile = "src/resources/opendata5524238908286670224.csv";
         BufferedReader bufferedReader;
         String line = "";
@@ -32,25 +31,51 @@ public class DataProc {
 
                 String[] dataPlace = line.split(splitWith);
 
-                Coord coordinate = new Coord(Double.parseDouble(dataPlace[0]), Double.parseDouble(dataPlace[1]));
-
-                pointList.add(new Point(coordinate, indexCalc(Objects.requireNonNull(downloadCSV(dataPlace[6])))));
-
+                pointList.add(new Point(Double.parseDouble(dataPlace[0]), Double.parseDouble(dataPlace[1]) ,
+                        indexCalc(Objects.requireNonNull(downloadCSV(dataPlace[7])))));
             }
 
         }
         catch (FileNotFoundException e){System.out.println("File not Found");}
         catch (IOException e){System.out.println("IOException");}
 
+        adaptMeasures(pointList);
+        for(Point point : pointList)
+            System.out.println(point.getData());
+
         return pointList;
     }
 
+    private void adaptMeasures(ArrayList<Point> pointList){
+        double max = 0.0;
+        double min = 100.0;
+
+        //Consigo el maximo de la lista
+        for(Point point : pointList){
+            double data = point.getData();
+            if(data > max){
+                max = point.getData();
+            }
+            else if(data < min){
+                min = data;
+            }
+        }
+
+        for(Point point : pointList){
+            double newValue = point.getData();
+            newValue = -(((newValue - min) / (max - min)) * 200 - 100);
+            point.setData((int)newValue);
+        }
+
+
+
+    }
+
     private static ArrayList<Integer> downloadCSV(String url){
-        ArrayList<Integer> result = new ArrayList<Integer>();
+        ArrayList<Integer> result = new ArrayList<>();
 
         BufferedInputStream inputStream;
         try {
-
             inputStream = new BufferedInputStream(new URL(url).openStream());
             DataInputStream dataInputStream = new DataInputStream(inputStream);
             String line = "";
@@ -65,58 +90,30 @@ public class DataProc {
             data = lastLine.split(separator);
 
             for(int i = 1; i < data.length; i++){
-                result.add(Integer.valueOf(data[i]));
+                if(!data[i].equals("") && data[i].matches("[0-9]+")) {
+                    result.add(Integer.valueOf(data[i]));
+                }
             }
-
-
         }
         catch (MalformedURLException e){System.out.println("MalformedURL");}
-        catch (IOException e){System.out.println("IOException downloading CSV"); return null;}
+        catch (IOException e){System.out.println(e.getMessage()); return null;}
 
         return result;
     }
 
     public int indexCalc(ArrayList<Integer> arrayList){
-        int result = 0;
-        for(int i = 0; i < arrayList.size(); i++){
-            switch (i){
-                case 0:
-                    result += arrayList.get(0) * 2;
-                    break;
-
-                case 1:
-                    result += arrayList.get(1) * 0.8;
-                    break;
-
-                case 2:
-                    break;
-
-
-                case 3:
-                    result += arrayList.get(3) * 0.5;
-                    break;
-
-                case 4:
-                    result += arrayList.get(4) * 2;
-                    break;
-
-                case 5:
-                    result += arrayList.get(5) * 0.5;
-                    break;
-
-                case 6:
-                    result += arrayList.get(6) * 0.833;
-                    break;
-
-            }
+        System.out.println(arrayList.toString());
+        double result = 0;
+        for (Integer integer : arrayList) {
+            result += integer;
         }
-        return result / 6;
+        result = result / arrayList.size();
+
+        return (int) result;
     }
 
     public static void main(String[] args){
-
-
+        DataProc dataProc = new DataProc();
     }
-
 
 }
