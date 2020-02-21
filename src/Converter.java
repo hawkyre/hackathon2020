3fe04aece8253836d8c33195d742cd1a26db5c29
airamhr9 <57371081+airamhr9@users.noreply.github.com>
2020-02-21 20:43:32 +0100

    Update Converter.java

--- src/Converter.java
+++ src/Converter.java
@@ -21,15 +21,4 @@ public class Converter {
         res.setY((c.getY() * relY) + despY);
         return res;
     }
-
-    public static void main(String[] args){/**
-        Coord viv = new Coord(39.480936,-0.367661);
-        Coord upvN = new Coord(2.5, 15);
-        Coord aytN = new Coord(5, 10);
-
-        Converter c = new Converter(upvN, aytN);
-        Point n = c.convert(viv);
-        System.out.println(n.getX());
-        System.out.println(n.getY());**/
-    }
 }
