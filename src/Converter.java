b2a4aedcce59b8dea8d380b3e8bb20a91aec8209
RodrigoLlanes <romadritex@gmail.com>
2020-02-21 17:35:38 +0100

    :)

--- src/Converter.java
+++ src/Converter.java
@@ -15,21 +15,21 @@ public class Converter {
         this.despY = upv.getY() - (upvCoord.getY() * this.relY);
     }
 
-    public Coord convert(Coord c) {
-        Coord res = new Coord(0, 0);
+    public Point convert(Point c) {
+        Point res = new Point(0, 0, c.getData());
         res.setX((c.getX() * relX) + despX);
         res.setY((c.getY() * relY) + despY);
         return res;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args){/**
         Coord viv = new Coord(39.480936,-0.367661);
         Coord upvN = new Coord(2.5, 15);
         Coord aytN = new Coord(5, 10);
 
         Converter c = new Converter(upvN, aytN);
-        Coord n = c.convert(viv);
+        Point n = c.convert(viv);
         System.out.println(n.getX());
-        System.out.println(n.getY());
+        System.out.println(n.getY());**/
     }
 }
